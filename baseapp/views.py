780884e564c214c74b11e35456c1aac2853995ae
Phichayaphak <ph_phich@hotmail.com>
2023-05-04 08:05:47 +0700

    dev 4/5/66

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.db.models import Max
 from django.shortcuts import render,redirect,HttpResponse, get_object_or_404
 from baseapp.models import *
 from baseapp.forms import *
+import os
+import D_Profile.settings
 
 def home(request):
     return render(request, 'home.html')
@@ -14,7 +18,27 @@ def personnelNew(request):
     if request.method == 'POST':
         form = PersonnelForm(data=request.POST, files=request.FILES)
         if form.is_valid():
-            form.save()
+            id_max = Personnel.objects.all().aggregate(Max('id'))['id__max']
+            id = id_max + 1 if id_max else 1
+            newForm = form.save(commit=False)
+            # id = newForm.id
+            # print("id: "+ str(id_next) )
+            # filename = newForm.picture.name
+            filepath = newForm.picture.name
+            filepath = filepath.replace(' ', '_')
+            point = filepath.rfind('.')
+            ext = filepath[point:]
+            filenames = filepath.split('/')
+            filename = filenames[len(filenames) - 1]
+            # filename = filename
+            newfilename = str(id) + ext
+            newForm.save()  # product_tmp/xxx.xxx
+            personnel = get_object_or_404(Personnel, id=id)
+            personnel.picture.name = 'images/personnels/' + newfilename  # pxxx.xxx
+            personnel.save()
+            if os.path.exists('static/images/personnels/' + newfilename):
+                os.remove('static/images/personnesl/' + newfilename)  # file exits, delete it
+            os.rename('static/images/personnels/'+filename, 'static/images/personnels/' + newfilename)
             return redirect('personnelList')
         else:
             context = {'form': form}
