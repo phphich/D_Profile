ed93e5f616062f79a989af4f3973524ba11f7b3b
Phichayaphak <ph_phich@hotmail.com>
2023-05-21 00:21:58 +0700

    dev 20/6/66

--- static/stylesheets/myJs.js
+++ static/stylesheets/myJs.js
@@ -15,14 +15,9 @@ updateList = function () {
 }
 /***************************************/
 
-/* ฟังก์ชัน check วันที่ ไม่ก่อนปัจจุบัน */
-/* ฟังก์ชัน check วันที่เริ่มต้น ต้องก่อนวันที่สิ้นสุด*/
-/* ฟังก์ชัน คำนวณจำนวนวัน */
-/* ฟังก์ชัน check จำนวนวัน ที่ไม่เกินวันที่สิ้นสุด - วันเริ่มต้น */
-/* ฟังก์ชัน check จำนวนเงิน ต้องเป็น 0 กรณีไม่ใช้งบประมาณ  */
 
-/* ฟังก์ชัน check งบประมาณ กรณีที่ไม่ใช้งบประมาณ */
-updateBudget = function () {
+/* ฟังก์ชัน check จำนวนเงิน ต้องเป็น 0 กรณีไม่ใช้งบประมาณ  */
+chkBudgetType = function () {
     var output = document.getElementById('id_budget');
     var input = document.getElementById('id_budgetType').selectedIndex;
     if (input == 3){
@@ -33,4 +28,40 @@ updateBudget = function () {
     }
 }
 
+/* ฟังก์ชัน check วันที่เริ่มต้น ต้องก่อนวันที่สิ้นสุด  */
+chkDateDiff = function () {
+    var output = document.getElementById('id_days');
+    var startDate = document.getElementById('id_startDate');
+    var endDate = document.getElementById('id_endDate');
+    var start_date = new Date(document.getElementById('id_startDate').value);
+    var end_date = new Date(document.getElementById('id_endDate').value);
+    if (end_date < start_date)  {
+        endDate.value = startDate.value
+        output.value =  1;
+    }else {
+        if (start_date && end_date) {
+            var time_difference = end_date.getTime() - start_date.getTime();
+            var days_difference = (time_difference / (1000*3600*24))+1;
+            output.value = days_difference;
+        } else{
+            output.value =  "";
+        }
+    }
+}
 
+/* ฟังก์ชัน คำนวณจำนวนวัน */
+chkDays = function () {
+    var output = document.getElementById('id_days');
+    var start_date = new Date(document.getElementById('id_startDate').value);
+    var end_date = new Date(document.getElementById('id_endDate').value);
+    var days = document.getElementById('id_days').value;
+    if (start_date && end_date) {
+        var time_difference = end_date.getTime() - start_date.getTime();
+        var days_difference = (time_difference / (1000 * 3600 * 24)) + 1;
+        // alert("Day: "+days.toString() + ", Diff: " + days_difference.toString());
+        if (days> days_difference) {
+            alert("ระบุจำนวนวันไม่ถูกต้อง");
+            output.value = days_difference;
+        }
+    }
+}
